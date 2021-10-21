d1492646a4804da3ce72e6975b277a527213c221
Ezequiel Avila Garcia <ezequiel.avila@gm2dev.com>
2021-10-21 19:13:11 -0300

    feat(core): add prisma schema and connection

--- src/post/models/post.model.ts
+++ src/post/models/post.model.ts
@@ -9,11 +9,11 @@ export class Post {
   @Field()
   text: string;
 
-  @Field((type) => Int)
-  userId: number;
-
   @Field()
   date: string;
+
+  @Field((type) => Int)
+  userId: number;
 
   @Field((type) => [Tag], { nullable: true }) // TODO: try to figure out how to avoid doing this optional. Its just to avoid deadlock in the responses. tag => posts, posts => tag
   tags?: Tag[];
