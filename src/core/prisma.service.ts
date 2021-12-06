cb12a9c8338ab71aad03391b10149f182603f7e3
anaibol <hey@anib.al>
2021-12-05 23:44:49 -0300

    chore: prisma query logging

--- src/core/prisma.service.ts
+++ src/core/prisma.service.ts
@@ -4,9 +4,43 @@ import { parse as uuidParse, stringify as uuidStringify } from 'uuid'
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: [
+        { emit: 'event', level: 'query' },
+        { emit: 'stdout', level: 'info' },
+        { emit: 'stdout', level: 'warn' },
+        { emit: 'stdout', level: 'error' },
+      ],
+    })
+  }
+
   // INFO: The onModuleInit is optional — if you leave it out, Prisma will connect lazily on its first call to the database. We don't bother with onModuleDestroy, since Prisma has its own shutdown hooks where it will destroy the connection. For more info on enableShutdownHooks, please see Issues with enableShutdownHooks
-  async onModuleInit() {
+  async onModuleInit(): Promise<void> {
     await this.$connect()
+
+    this.$use(async (params, next) => {
+      const before = Date.now()
+
+      const result = await next(params)
+
+      const after = Date.now()
+
+      console.log(`Query ${params.model}.${params.action} took ${after - before}ms`)
+
+      return result
+    })
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    this.$on('query', async (e) => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      console.log('Query: ' + e.query)
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      console.log('Duration: ' + e.duration + 'ms')
+    })
   }
 
   async enableShutdownHooks(app: INestApplication) {
