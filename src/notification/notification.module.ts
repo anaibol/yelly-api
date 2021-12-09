5467c5674d963a1d9eabcb6170591f3026c5ca63
Juan <juanobrach@gmail.com>
2021-12-09 19:02:19 -0300

    feat(1491): add /notifications/chat-messages endpoint

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -4,10 +4,12 @@ import { CoreModule } from '../core/core.module'
 import { UserModule } from '../user/user.module'
 import { NotificationResolver } from './notification.resolver'
 import { NotificationService } from './notification.service'
+import { ControllerController } from './controller/controller.controller'
 
 @Module({
   imports: [CoreModule, AuthModule, UserModule],
   providers: [NotificationService, NotificationResolver],
   exports: [NotificationService],
+  controllers: [ControllerController],
 })
 export class NotificationModule {}
