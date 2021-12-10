8afb3ed0407152cd6276e1ae86d7078c88d4ab38
Juan <juanobrach@gmail.com>
2021-12-10 11:19:18 -0300

    chore(1491): change folder name and return status code

--- src/sendbird-webhook/sendbird-webhook.module.ts
+++ src/sendbird-webhook/sendbird-webhook.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common'
 import { CoreModule } from 'src/core/core.module'
-import { PushNotificationController } from './push-notification.controller'
-import { PushNotificationService } from './push-notification.service'
+import { SendbirdWebhookController } from './sendbird-webhook.controller'
+import { PushNotificationService } from '../core/push-notification.service'
 
 @Module({
   imports: [CoreModule],
   providers: [PushNotificationService],
-  controllers: [PushNotificationController],
+  controllers: [SendbirdWebhookController],
 })
 export class PushNotificationModule {}
