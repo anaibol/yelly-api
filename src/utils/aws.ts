e218132c73418ed697b47863f3b38381f66c5fab
DevPolo <paul.crussaire@gmail.com>
2021-12-15 11:44:35 +0100

    fix: replace get by put object command on aws

--- src/utils/aws.ts
+++ src/utils/aws.ts
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid'
 
-import { S3Client, GetObjectCommand, S3ClientConfig } from '@aws-sdk/client-s3'
+import { S3Client, S3ClientConfig, PutObjectCommand } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 
 const REGION = 'eu-west-3'
@@ -15,10 +15,14 @@ const s3Configuration: S3ClientConfig = {
 }
 const s3 = new S3Client(s3Configuration)
 
-const getPresignedUrl = async (): Promise<string> => {
-  return getSignedUrl(s3, new GetObjectCommand({ Bucket: BUCKET, Key: uuidv4() }), {
-    expiresIn: 15 * 60, // expires in seconds
-  })
-}
+export type IGetUploadPresignedUrl = '/test' | '/avatar'
 
-export default { getPresignedUrl }
+export const getPresignedUrl = async (folder?: IGetUploadPresignedUrl): Promise<string> => {
+  const id: string = uuidv4()
+  const key: string = folder ? folder + id : id
+
+  const command = new PutObjectCommand({ Bucket: BUCKET, Key: key })
+  const options = { expiresIn: 15 * 60 } // expires after 15 minutes
+
+  return getSignedUrl(s3, command, options)
+}
