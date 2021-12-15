107dbade731644a010fbcc70a47c986e674ed614
DevPolo <paul.crussaire@gmail.com>
2021-12-15 12:42:23 +0100

    feat: resolver decoration in upload resolver

--- src/user/upload.resolver.ts
+++ src/user/upload.resolver.ts
@@ -1,7 +1,8 @@
-import { Mutation } from '@nestjs/graphql'
+import { Mutation, Resolver } from '@nestjs/graphql'
 
 import { getPresignedUrl } from '../utils/aws'
 
+@Resolver()
 export class UploadResolver {
   @Mutation(() => String)
   async getS3PresignedUrls() {
