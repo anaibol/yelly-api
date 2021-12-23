a982e26dfc4897b2be837d489c9dcc15560e2b6a
anaibol <hey@anib.al>
2021-12-23 13:26:43 -0300

    chore: use nest-cli to generate fields

--- src/user/reset-password.input.ts
+++ src/user/reset-password.input.ts
@@ -1,9 +1,7 @@
-import { Field, InputType } from '@nestjs/graphql'
+import { InputType } from '@nestjs/graphql'
 
 @InputType()
 export class ResetPasswordInput {
-  @Field()
   password: string
-  @Field()
   resetToken: string
 }
--- src/user/update-user.input.ts
+++ src/user/update-user.input.ts
@@ -1,31 +1,18 @@
-import { Field, InputType } from '@nestjs/graphql'
+import { InputType } from '@nestjs/graphql'
 
 @InputType()
 export class UpdateUserInput {
-  @Field()
   firstName?: string
-  @Field()
   lastName?: string
-  @Field()
   email?: string
-  @Field()
   password?: string
-  @Field()
   birthdate?: Date
-  @Field()
-  pictureId?: string
-  @Field()
-  avatar3dId?: string
-  @Field()
-  trainingName?: string
-  @Field()
-  schoolGooglePlaceId?: string
-  @Field()
-  about?: string
-  @Field()
-  isFilled?: boolean
-  @Field()
-  snapchat?: string
-  @Field()
   instagram?: string
+  snapchat?: string
+  pictureId?: string
+  avatar3dId?: string
+  trainingName?: string
+  schoolGooglePlaceId?: string
+  about?: string
+  isFilled?: boolean
 }
