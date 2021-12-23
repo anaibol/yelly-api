554b9dac52106a4f0494efa81a84d944b99c2bec
anaibol <hey@anib.al>
2021-12-23 10:16:44 -0300

    fix: script migrate token

--- scripts/migrateExpoToken.ts
+++ scripts/migrateExpoToken.ts
@@ -1,40 +1,40 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
-async function main() {
-  const prisma = new PrismaClient()
+// async function main() {
+//   const prisma = new PrismaClient()
 
-  let hasUsers = true
-  let skip = 0
-  while (hasUsers) {
-    const users = await prisma.user.findMany({
-      where: {
-        NOT: {
-          expoPushNotificationToken: null,
-        },
-      },
-      select: {
-        id: true,
-        expoPushNotificationToken: true,
-      },
-      skip: skip,
-      take: 500,
-    })
-    if (users.length == 0) {
-      hasUsers = false
-      console.log('finish')
-      return
-    }
-    skip += 500
+//   let hasUsers = true
+//   let skip = 0
+//   while (hasUsers) {
+//     const users = await prisma.user.findMany({
+//       where: {
+//         NOT: {
+//           expoPushNotificationToken: null,
+//         },
+//       },
+//       select: {
+//         id: true,
+//         expoPushNotificationToken: true,
+//       },
+//       skip: skip,
+//       take: 500,
+//     })
+//     if (users.length == 0) {
+//       hasUsers = false
+//       console.log('finish')
+//       return
+//     }
+//     skip += 500
 
-    console.log('insert ' + skip)
+//     console.log('insert ' + skip)
 
-    const userData = users.map((user) => {
-      return { userId: user.id, token: user.expoPushNotificationToken }
-    })
-    await prisma.expoPushNotificationAccessToken.createMany({
-      data: userData,
-    })
-  }
-}
+//     const userData = users.map((user) => {
+//       return { userId: user.id, token: user.expoPushNotificationToken }
+//     })
+//     await prisma.expoPushNotificationAccessToken.createMany({
+//       data: userData,
+//     })
+//   }
+// }
 
-main()
+// main()
