8f1d4ff7daa9b2ef5bf57b5fce63cc7de32e4591
Anibal <hey@anib.al>
2022-03-22 16:37:03 -0300

    fix: restore 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
 
-  await app.listen(8080, process.env.NODE_ENV === 'development' ? 'localhost' : '0.0.0.0')
+  await app.listen(8080, '0.0.0.0')
 }
 
 bootstrap()
