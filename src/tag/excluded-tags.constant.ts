9fb4448aca0eda42cd021c6297d8f08f52d241fd
Anibal <hey@anib.al>
2022-04-18 23:19:33 -0300

    feat: add more excluded tags

--- src/tag/excluded-tags.constant.ts
+++ src/tag/excluded-tags.constant.ts
@@ -1,1 +1,14 @@
-export const excludedTags = ['instagram', 'insta', 'snap', 'snapchat', 'insta/snap', 'tiktok', 'celiboucouple']
+export const excludedTags = [
+  'instagram',
+  'insta',
+  'snap',
+  'snapchat',
+  'insta/snap',
+  'tiktok',
+  'celiboucouple',
+  'venezparler',
+  'quipourparler',
+  'rencontre',
+  'connaissance',
+  'celib',
+]
