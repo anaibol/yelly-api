cf70da1bedec7d7f9f41d04298007d3af3b84c27
Anibal <hey@anib.al>
2022-06-13 10:18:28 -0300

    feat: add tag author

--- src/feed/feed.module.ts
+++ src/feed/feed.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common'
 import { AuthModule } from '../auth/auth.module'
 import { CoreModule } from '../core/core.module'
+import { TagModule } from '../tag/tag.module'
 import { UserModule } from '../user/user.module'
 import { FeedResolver } from './feed.resolver'
 import { FeedService } from './feed.service'
 
 @Module({
-  imports: [CoreModule, AuthModule, UserModule],
+  imports: [CoreModule, AuthModule, UserModule, TagModule],
   providers: [FeedService, FeedResolver],
   exports: [FeedService],
 })
