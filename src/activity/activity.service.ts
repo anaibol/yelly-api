d4ff16e049269441ff1cf1126d196fdc2a804412
Anibal <hey@anib.al>
2022-07-28 23:35:17 -0300

    fix: add lastname

--- src/activity/activity.service.ts
+++ src/activity/activity.service.ts
@@ -127,7 +127,9 @@ export class ActivityService {
             select: {
               id: true,
               firstName: true,
+              lastName: true,
               pictureId: true,
+              birthdate: true,
             },
           },
           tag: {
