d92e8b96f2a7dad5f2df4fcc3f421592fcfcd69d
Anibal <juananibal.micheli@gmail.com>
2022-09-20 12:47:18 -0300

    fix: merge

--- prisma/migrations/20220915171113_/migration.sql
+++ prisma/migrations/20220915171113_/migration.sql
@@ -5,10 +5,16 @@
 
 */
 -- AlterTable
-ALTER TABLE "User" ALTER COLUMN "displayName" DROP NOT NULL,
-ALTER COLUMN "displayName" DROP DEFAULT,
-ALTER COLUMN "username" DROP NOT NULL,
-ALTER COLUMN "username" DROP DEFAULT;
-
+ALTER TABLE "User"
+ALTER COLUMN "displayName" DROP NOT NULL,
+  ALTER COLUMN "displayName" DROP DEFAULT,
+  ALTER COLUMN "username" DROP NOT NULL,
+  ALTER COLUMN "username" DROP DEFAULT;
+UPDATE "User"
+SET "username" = NULL
+WHERE "username" = '';
+UPDATE "User"
+SET "displayName" = NULL
+WHERE "displayName" = '';
 -- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
